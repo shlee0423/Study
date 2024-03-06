94448de81203d8c851474a129727d9e8413cc64c
shlee0423 <128872754+shlee0423@users.noreply.github.com>
2024-03-06 18:53:45 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 3.30 ms, Memory: 96.5 MB -BaekjoonHub

--- 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.java
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.java
@@ -1,15 +1,13 @@
 class Solution {
     public int[] solution(int[] arr, int k) {
-        if(k%2==0){
-            for(int i=0; i<arr.length; i++){
-                arr[i]+=k;
+        for(int i=0; i<arr.length; i++){
+                if(k%2==0){
+                    arr[i]+=k;
+                }
+                else{
+                    arr[i]*=k;
+                }
             }
-        }
-        else{
-            for(int i=0; i<arr.length; i++){
-                arr[i]*=k;
-            }
-        }
         return arr;
     }
 }
